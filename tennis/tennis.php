c6a209b6e2f4ac03261e4ca8cfa9f85252fbd803
Christof Damian <christof@damian.net>
2012-03-15 21:28:50 +0100

    tennis kata

---
+++ tennis/tennis.php
@@ -0,0 +1,58 @@
+<?php
+
+class Tennis {
+
+    private $points = array();
+
+    private $scoring = array( 0, 15, 30, 40 );
+
+    private $winner;
+
+    public $advantaged;
+
+    public function __construct()
+    {
+        $this->points = array(
+            1 => 0,
+            2 => 0,
+        );
+    }
+
+    public function givePoint( $player )
+    {
+        if( $this->winner ) return false;
+
+
+        if( 40 == $this->getPoints(1) && 40 == $this->getPoints(2) )
+        {
+
+                if ($this->advantaged == $player ) {
+                    $this->winner = $player;
+                    return;
+                } elseif ($this->advantaged ) {
+                    $this->advantaged = null;
+                }
+                else $this->advantaged = $player;
+        }
+        else {
+
+            $this->points[$player]++;
+
+            if( $this->points[$player] >= count( $this->scoring) )
+            {
+                $this->winner = $player;
+            }
+        }
+
+    }
+
+    public function getPoints( $player )
+    {
+        return $this->scoring[ $this->points[$player] ];
+    }
+
+    public function getWinner ()
+    {
+        return $this->winner;
+    }
+}
---
+++ tennis/tennisTest.php
@@ -0,0 +1,110 @@
+<?php
+
+require_once 'tennis.php';
+
+class TennisTest extends PHPUnit_Framework_TestCase {
+
+    public function setUp() {
+
+        $this->obj = new Tennis();
+    }
+
+    public function testPoint() {
+        $this->assertNull($this->obj->givePoint(1));
+    }
+
+    public function testGetPoints() {
+
+        $this->obj->givePoint(1);
+        $this->assertEquals( 15, $this->obj->getPoints(1));
+        $this->assertEquals( 0, $this->obj->getPoints(2));
+    }
+
+    public function testGet3Points() {
+
+        $this->obj->givePoint(1);
+        $this->obj->givePoint(1);
+        $this->obj->givePoint(1);
+
+        $this->assertEquals( 40, $this->obj->getPoints(1));
+    }
+
+    public function testGetWinner1() {
+
+        $this->obj->givePoint(1);
+        $this->obj->givePoint(1);
+        $this->obj->givePoint(1);
+        $this->obj->givePoint(1);
+
+        $this->assertEquals( 1, $this->obj->getWinner());
+    }
+
+    public function testGetWinner2() {
+
+        $this->obj->givePoint(2);
+        $this->obj->givePoint(2);
+        $this->obj->givePoint(2);
+        $this->obj->givePoint(2);
+
+        $this->assertEquals( 2, $this->obj->getWinner());
+    }
+
+    public function testGetNoWinner() {
+
+        $this->obj->givePoint(1);
+        $this->obj->givePoint(1);
+        $this->obj->givePoint(1);
+
+        $this->assertNull( $this->obj->getWinner() );
+    }
+
+    public function testGetNoWinnerBecauseAdvantage() {
+
+        $this->obj->givePoint(1);
+        $this->obj->givePoint(1);
+        $this->obj->givePoint(1);
+        $this->obj->givePoint(2);
+        $this->obj->givePoint(2);
+        $this->obj->givePoint(2);
+        $this->obj->givePoint(2);
+
+        $this->assertNull( $this->obj->getWinner() );
+    }
+
+    public function testGetNoWinnerAfter5hoursMatch() {
+
+        $this->obj->givePoint(1);
+        $this->obj->givePoint(1);
+        $this->obj->givePoint(1);
+        $this->obj->givePoint(2);
+        $this->obj->givePoint(2);
+        $this->obj->givePoint(2);
+
+        $this->obj->givePoint(2);
+        $this->obj->givePoint(1);
+        $this->obj->givePoint(2);
+        $this->obj->givePoint(1);
+
+        $this->assertNull( $this->obj->getWinner() );
+    }
+
+    public function testGetWinner1After5hoursMatch() {
+
+        $this->obj->givePoint(1);
+        $this->obj->givePoint(1);
+        $this->obj->givePoint(1);
+        $this->obj->givePoint(2);
+        $this->obj->givePoint(2);
+        $this->obj->givePoint(2);
+
+        $this->obj->givePoint(2);
+        $this->obj->givePoint(1);
+        $this->obj->givePoint(2);
+        $this->obj->givePoint(1);
+
+        $this->obj->givePoint(1);
+        $this->obj->givePoint(1);
+
+        $this->assertEquals( 1, $this->obj->getWinner() );
+    }
+}
